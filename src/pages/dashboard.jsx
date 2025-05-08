48c5125368054175ab1e0ded1cc9e076e0afb68b
shrutitaylor <shruti128770@gmail.com>
2025-05-08 12:49:36 +1000

    edit date

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -693,7 +693,7 @@ const handleReset = () => {
                       name={getDisplayName(key)}
                       value={value}
                       onChange={handleChange}
-                      disabled={key === "date" || key === "ticketNo"}
+                      // disabled={key === "date" || key === "ticketNo"}
                       className={`w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 font-spaceGrotesk ${
                         (key === "date" || key === "ticketNo") ? "bg-gray-100" : ""
                       }`}
