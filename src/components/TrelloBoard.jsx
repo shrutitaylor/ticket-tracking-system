27d8e67b40224c8effca6415ab429e65d99557c3
shrutitaylor <shruti128770@gmail.com>
2025-08-01 14:02:32 +1000

    Columns added - trello board

--- src/components/TrelloBoard.jsx
+++ src/components/TrelloBoard.jsx
@@ -22,10 +22,14 @@ const TrelloBoard = (props) => {
   const [columns, setColumns] = useState(groupByStatus(props.paginatedTickets));
 //   const statusOrder = ["To Do", "In Progress", "Review", "Done"];
   const statusOrder = [
+    "EMERGENCY",
     "Return with update",
-    "Waiting for Parts",
+    "Waiting for Customer",
+  "Waiting for Parts",
+  "Waiting for Device",
     "Sent to Mike",
-    "Pending Payment"];
+    "Pending Payment",
+  "Send Invoice",];
 
     const onDragEnd = async (result) => {
     const { source, destination } = result;
@@ -62,18 +66,21 @@ const TrelloBoard = (props) => {
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div className="flex font-mono grid md:grid-cols-4 grid-cols-2 gap-4 justify-center mx-auto p-4">
+      <div className="flex font-mono grid md:grid-cols-4 md:grid-rows-2 grid-cols-2 gap-4 justify-center mx-auto p-4">
         {statusOrder.map((status) => (
           <Droppable droppableId={status} key={status}>
             {(provided, snapshot) => (
               <div
                 ref={provided.innerRef}
                 {...provided.droppableProps}
-                className={`flex-shrink-0 relative h-44 md:h-auto overflow-auto rounded-lg shadow p-3 transition-all duration-300 ${
+                className={`flex-shrink-0 relative h-44 md:h-80 overflow-auto rounded-lg shadow p-3 transition-all duration-300 ${
                   snapshot.isDraggingOver ? "bg-lime-400" : "bg-stone-100"
-                }`}
+                }
+                ${status==="EMERGENCY" ? "bg-gradient-to-r from-rose-400 to-red-400 text-stone-100":"bg-stone-200 text-stone-700"}
+                `}
+                 
               >
-                <h2 className="text-stone-700 font-bold text-sm md:text-lg mb-3">{status}</h2>
+                <h2 className=" font-bold text-sm md:text-lg mb-3">{status}</h2>
                 <div className="flex flex-col gap-3 min-h-[50px]">
                   {(columns[status] || []).map((ticket, index) => (
                     <Draggable key={ticket.id} draggableId={ticket.id.toString()} index={index}>
@@ -82,7 +89,8 @@ const TrelloBoard = (props) => {
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
-                          className={`bg-stone-200 rounded p-3 text-stone-800 shadow-sm border-l-4 ${
+                          className={`bg-stone-100 rounded p-3 text-stone-800 shadow-sm border-l-4
+                             ${
                             ticket.priority === "H"
                               ? "border-rose-400"
                               : ticket.priority === "M"
